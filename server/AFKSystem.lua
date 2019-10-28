51a35dc97043064c487297dd66b3c8b83795bf6d
Hallkezz <32846474+Hallkezz@users.noreply.github.com>
2019-10-28 22:44:43 +0300

    Update AFKSystem.lua

--- server/AFKSystem.lua
+++ server/AFKSystem.lua
@@ -15,25 +15,32 @@ class 'AFKSystem'
 function AFKSystem:__init()
 	Events:Subscribe( "PlayerJoin", self, self.PlayerJoin )
 	Events:Subscribe( "PlayerChat", self, self.PlayerChat )
+	Network:Subscribe( "ToggleAFKTeg", self, self.ToggleAFKTeg )
 end
 
 function AFKSystem:PlayerJoin( args )
-	if self.onjoin then
-		Network:Send( args.player, "AFKSystem", true)
-	end
+	args.player:SetNetworkValue( "PlayerAFK", nil )
 end
 
 function AFKSystem:PlayerChat( args )
-    local cmd_args = args.text:split( " " )
+	local cmd_args = args.text:split( " " )
 
-    if cmd_args[1] == active or cmd_args[1] == activeTwo then
-		Network:Send( args.player, "AFKSystem", false)
-        return false
-    end
-    return true
+	if cmd_args[1] == active or cmd_args[1] == activeTwo then
+		Network:Send( args.player, "AFKSystem" )
+		return false
+	end
+	return true
+end
+
+function AFKSystem:ToggleAFKTeg( args, sender )
+	if sender:GetValue( "PlayerAFK" ) then
+		sender:SetNetworkValue( "PlayerAFK", false )
+	else
+		sender:SetNetworkValue( "PlayerAFK", true )
+	end
 end
 
 afksystem = AFKSystem()
 
---v1.1--
---04.11.18--
+--v2--
+--28.10.19--
